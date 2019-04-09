976a245539af2dba1041bfba3bb82e908d41892d
MayankValechha <valechhamayank12@gmail.com>
2019-04-10 03:35:01 +0530

    Send Jobsheets to database

--- createJobsheet.php
+++ createJobsheet.php
@@ -15,8 +15,49 @@
         $shop_info = mysqli_fetch_assoc($result);
     }
 
+    //Checking if Jobsheet is Submitted
     if(isset($_POST['submit_job'])) {
-        echo 'Submitted';
+
+        //Grabbing all form fields
+        $shop_ID = $_SESSION['shopID'];
+        $customer_name = mysqli_real_escape_string($db_connect, $_POST['customer_name']);
+        $customer_contact = mysqli_real_escape_string($db_connect, $_POST['customer_contact']);
+        $customer_email = mysqli_real_escape_string($db_connect, $_POST['customer_email']);
+
+        $mobile_model = mysqli_real_escape_string($db_connect, $_POST['mobile_model']);
+        $mobile_imei = mysqli_real_escape_string($db_connect, $_POST['mobile_imei']);
+        $other_things = mysqli_real_escape_string($db_connect, $_POST['other_things']);
+        $password_of_mobile = mysqli_real_escape_string($db_connect, $_POST['password_of_mobile']);
+        $condition_of_mobile = mysqli_real_escape_string($db_connect, $_POST['condition_of_mobile']);
+        
+        $problem = mysqli_real_escape_string($db_connect, $_POST['problem_in_mobile1']);
+        $amount = mysqli_real_escape_string($db_connect, $_POST['estimate1']);
+
+        $query = "
+                    INSERT INTO `jobsheet`
+                    (customer_name, 
+                    customer_contact, 
+                    customer_email, 
+                    imei, 
+                    model, 
+                    password, 
+                    other_things, 
+                    condition_of_mobile,
+                    problem_description, 
+                    estimated_amount, shop_id)
+                    VALUES
+                    ('$customer_name', '$customer_contact', '$customer_email', '$mobile_model', '$mobile_imei', '$password_of_mobile', '$other_things', '$condition_of_mobile','$problem', '$amount', '$shop_ID')";
+                            
+        if(!mysqli_query($db_connect, $query)) 
+        {
+            die('Error : '.mysqli_errno($db_connect));
+        }
+        else 
+        {
+            header('Location: dashboard.php');
+            exit();
+        }
+
     }
 
 ?>
@@ -29,9 +70,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
     <title>Create Jobsheet | Job Sheet Application</title>
-    
-    <style>
 
+    <style>
         .wrapper {
             border:2px solid black;
             padding:0;
@@ -90,6 +130,7 @@
                         <!-- 100% Width of Inputs -->
                         <input type="text" name="customer_name" class="form-control" placeholder="Customer Name">
                         <input type="text" name="customer_contact" class="form-control" placeholder="Customer Contact">
+                        <input type="text" name="customer_email" class="form-control" placeholder="Customer Email">
                         
                         <!-- 50% Width of Inputs -->
                         <div class="form-inline">
@@ -112,11 +153,11 @@
                     <!-- START OF FORM ROW -->
                     <div class="row">
                         <div class="col-md-8 col-lg-9 col-sm-6 col-xs-6" style="padding-right:0;">
-                            <input type="text" name="problem_in_mobile" class="form-control" placeholder="Problem">
-                            <input type="text" name="problem_in_mobile" class="form-control" placeholder="">
-                            <input type="text" name="problem_in_mobile" class="form-control" placeholder="">
-                            <input type="text" name="problem_in_mobile" class="form-control" placeholder="">
-                            <input type="text" name="problem_in_mobile" class="form-control" placeholder="">
+                            <input type="text" name="problem_in_mobile1" class="form-control" placeholder="Problem">
+                            <input type="text" name="problem_in_mobile2" class="form-control" placeholder="">
+                            <input type="text" name="problem_in_mobile3" class="form-control" placeholder="">
+                            <input type="text" name="problem_in_mobile4" class="form-control" placeholder="">
+                            <input type="text" name="problem_in_mobile5" class="form-control" placeholder="">
                         </div>
                         <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3" style="padding-left:0;">
                             <input type="text" name="estimate1" class="form-control" placeholder="Amount">
@@ -127,6 +168,7 @@
                         </div>
                     </div>
 
+                    
                     <input type="submit" value="Submit Jobsheet" name="submit_job" class="btn btn-primary">
                     <!-- END OF FORM ROW -->
                 </form>
