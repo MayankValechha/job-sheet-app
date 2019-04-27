d0cdc6248822df9f05bf99ba28ff68ba51750bbe
MayankValechha <valechhamayank12@gmail.com>
2019-04-27 12:48:43 +0530

    Contains include files

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<style>
+    footer {
+        padding: 15px;
+        font-size: 14px;
+        text-align: center;
+        background-color: #3c3d41;
+        color: rgba(255,255,255,1.00);
+    }
+
+    .hr {
+        border-bottom: 2px dotted orange;
+        width:30%;
+        margin: auto;
+        margin-bottom:10px;
+    }
+
+    p span {
+        font-weight: bold;
+        color:white;
+    }
+
+    p {
+        margin: 0 0 8px 0;
+    }
+</style>
+<footer>
+    <h4><b>Job Sheet Application</b></h4>
+    <div class="hr"></div>
+    <p>Developed and Designed by <span> &copy; Mayank Valechha.</span><b>As a Part of My Minor Project in 3rd Year!</b></p>
+    <p><span>Swami Vivekanand College of Engineering, Indore</span></p>
+    <p><span>e-mail : </span>mayank.valechha@yahoo.com</p> 
+</footer>
