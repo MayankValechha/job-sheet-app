b96cc45cd652d9790a30ded72cde388ef8bab095
MayankValechha <valechhamayank12@gmail.com>
2019-04-29 02:09:43 +0530

    Under Developement

--- dashboard.php
+++ dashboard.php
@@ -66,14 +66,9 @@
         <h3><a href="logout.php">Logout</a></h3>
     <?php endif; ?>
 
-    <!-- If user is not logged In! -->
-    <?php if(!isset($_SESSION['username'])): ?>
-        <p class="alert alert-danger text-center"><?php echo "You need to login first to access this page!"; ?></p>
-        <img src="imgs/loading 5.gif" style="display: block; margin: auto;">
-        <h3 class="text-center">Redirecting to Login Page in 5 seconds!</h3>
-        <?php header('Refresh: 6; url=login.php'); ?>
-    <?php endif;?>
-    
+    <!-- If user is not logged in, below code will run -->
+    <?php include 'includes/usernot_set.php'; ?>
+
     <!-- Testing Purpose -->    
     <!-- Will be deleted later -->
     <div class="bg-dark text-white">
