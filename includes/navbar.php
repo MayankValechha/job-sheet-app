d313c2956f3dffcec49065b96b22df99c18dfc33
MayankValechha <valechhamayank12@gmail.com>
2019-04-30 03:58:54 +0530

    All include files

---
+++ includes/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand font-weight-bold" href="#">Job Sheet Application</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="dashboard.php">Dashboard <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="createJobsheet.php">Create Jobsheet</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="viewJobsheets.php">View Jobsheets</a>
+            </li>
+        </ul>
+        <div class="form-inline my-2 my-lg-0">
+            <a href="logout.php" class="btn btn-secondary">Logout</a>
+        </div>
+    </div>
+</nav>
